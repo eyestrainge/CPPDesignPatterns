026ef8184dc42efb6d595899ffdc7c467998df77
sivaniadze <saba.ivaniadze@gmail.com>
2020-03-17 22:41:14 +0400

    Template Method

---
+++ Behavioral/TemplateMethod/TemplateMethod/TemplateMethod/src/TemplateMethod.cpp
@@ -0,0 +1,63 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+struct Game
+{
+	explicit Game(const int number_of_players) :
+		number_of_players{ number_of_players }
+	{}
+
+	void run()
+	{
+		start();
+		while (!have_winner())
+			take_turn();
+		cout << "Player " << get_winner() << " wins.\n";
+	}
+
+protected:
+	int number_of_players;
+	int current_player{ 0 };
+
+	virtual void start() = 0;
+	virtual bool have_winner() = 0;
+	virtual void take_turn() = 0;
+	virtual int get_winner() = 0;
+};
+
+struct Chess : Game
+{
+	explicit Chess() : Game{ 2 } {}
+
+protected:
+	void start() override
+	{
+		cout << "Starting a game of chess with " << number_of_players << " players.\n";
+	}
+	bool have_winner() override
+	{
+		return turns == max_turns;
+	}
+	void take_turn() override
+	{
+		cout << "Turn " << turns << " taken by player " << current_player << "\n";
+		turns++;
+		current_player = (current_player + 1) % number_of_players;
+	}
+	int get_winner() override
+	{
+		return current_player;
+	}
+
+private:
+	int turns{ 0 }, max_turns{ 10 };
+};
+
+int main()
+{
+	Chess chess;
+	chess.run();
+
+	return 0;
+}
